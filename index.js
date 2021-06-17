fbd8ee906857dfe49d8e3806a0779bdc8d1208b3
Jesulino Macedo <jesulino.macedo@cepedi.org.br>
2021-06-17 02:23:21 -0300

    novos endpoints para lógica de eleição

--- index.js
+++ index.js
@@ -6,14 +6,38 @@ const app = express()
 app.use(express.json())
 
 var ocupado = false
+var myInfo = {
+  "componente": "server",
+  "descrição": "Serve os clientes com os serviços X, Y e Z",
+  "versao": "0.2",
+  "ponto_de_acesso": "http://sd-app-server-jesulino.herokuapp.com",
+  "status": "up",
+  "identificacao": 7,
+  "lider": 0,
+  "eleicao": "valentao",
+  "servidores_conhecidos": []
+}
+var infos = []
+var coordenadorAtual = undefined
+var eleicao = {
+  "id": undefined,
+  "ativo": false
+}
 
 app.get('/info', (req, resp) => {
-  const result = {
-    "componente": "server",
-    "descrição": "Serve os clientes com os serviços X, Y e Z",
-    "versao": "0.1"
-  }
-  resp.send(result)
+  resp.send(myInfo)
+})
+
+app.post('/info', (req, resp) => {
+  myInfo.status = req.body.status || myInfo.status
+  myInfo.identificacao = req.body.identificacao ?? myInfo.identificacao
+  myInfo.lider = req.body.lider ?? myInfo.lider
+  myInfo.eleicao = req.body.eleicao || myInfo.eleicao
+  resp.send({ "mensagem": "Info atualizada com sucesso!", "novaInfo": myInfo })
+})
+
+app.get('/recurso', (req, resp) => {
+  resp.send({ "ocupado": ocupado })
 })
 
 app.post('/recurso', (req, resp) => {
@@ -25,6 +49,22 @@ app.post('/recurso', (req, resp) => {
     setTimeout(() => ocupado = false, 10000)
     resp.send({ 'message': 'Recurso alocado com sucesso!'});
   }
+})
+
+app.get('/eleicao', (req, resp) => {
+  resp.send({ "tipo_de_eleicao_ativa": myInfo.eleicao, "eleicao_em_andamento": eleicao.ativo })
+})
+
+app.post('/eleicao', (req, resp) => {
+  eleicao.id = req.body.id
+  eleicao.ativo = true
+  setTimeout(() => eleicao.ativo = false, 10000)
+  resp.send({ "message": "Eleição iniciada!" })
+})
+
+app.post('/eleicao/coordenador', (req, resp) => {
+  coordenadorAtual = req.body.coordenador
+  resp.send({ "message": "Coordenador atualizado!" })
 })
 
 const port = process.argv[2]
