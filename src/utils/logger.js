a03f79055cd5c1e6f9800f07f6264e4f8f44c8a2
Jesulino Macedo <jesulino.macedo@cepedi.org.br>
2021-06-19 22:13:16 -0300

    alterando formatação dos logs em função do heroku

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,29 +2,14 @@ export default class Logger {
   constructor() { }
 
   static error(mensagem) {
-    const now = new Date(Date.UTC() - 3 * 60 * 60 * 1000);
-    const nowString = formatDate(now)
-    console.log(`\x1b[31m[ERROR]\x1b[0m ${nowString} ${mensagem}`)
+    console.error(`${mensagem}`)
   }
 
   static info(mensagem) {
-    const now = new Date(Date.now() - 3 * 60 * 60 * 1000);
-    const nowString = formatDate(now)
-    console.log(`\x1b[34m[INFO]\x1b[0m ${nowString} ${mensagem}`)
+    console.log(`${mensagem}`)
   }
 
   static warn(mensagem) {
-    const now = new Date(Date.UTC() - 3 * 60 * 60 * 1000);
-    const nowString = formatDate(now)
-    console.log(`\x1b[33m[WARN]\x1b[0m ${nowString} ${mensagem}`)
+    console.warn(`${mensagem}`)
   }
-}
-
-const formatDate = (date) => {
-  return date.getUTCFullYear() + "-" +
-    ("0" + (date.getUTCMonth()+1)).slice(-2) + "-" +
-    ("0" + date.getUTCDate()).slice(-2) + " " +
-    ("0" + date.getUTCHours()).slice(-2) + ":" +
-    ("0" + date.getUTCMinutes()).slice(-2) + ":" +
-    ("0" + date.getUTCSeconds()).slice(-2) + "-03:00";
 }
