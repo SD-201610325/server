88db1eb1d7e05b036109146eddb67677b52361a4
Jesulino Macedo <jesulino.macedo@cepedi.org.br>
2021-06-27 14:00:51 -0300

    revertendo mudança na coloração dos logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,14 +2,14 @@ export default class Logger {
   constructor() { }
 
   static error(mensagem) {
-    console.error(` -- [ERROR] -- ${mensagem}`)
+    console.error(`\x1b[31m[ERROR]\x1b[0m ${mensagem}`)
   }
 
   static info(mensagem) {
-    console.log(` -- [INFO] -- ${mensagem}`)
+    console.log(`\x1b[34m[INFO]\x1b[0m ${mensagem}`)
   }
 
   static warn(mensagem) {
-    console.warn(` -- [WARN] -- ${mensagem}`)
+    console.warn(`\x1b[33m[WARN]\x1b[0m ${mensagem}`)
   }
 }
